d5fdff468ecab25359d7718fec229829782cefcc
SebasiaanJamaer <sebastiaan.jamaer@hotmail.com>
2018-11-12 16:19:07 +0100

    create report SVM

--- SVM-retraining/crack_scikit.py
+++ SVM-retraining/crack_scikit.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.datasets import make_circles
+from sklearn.datasets import make_blobs
 from sklearn import svm
 from sklearn.metrics import accuracy_score
 from Adversary import Adversary
@@ -14,12 +15,18 @@ number of queries than the number of initial training points.
 
 def main(budget, rounds):
 	X1, Y1 = make_circles(n_samples=800, noise=0.1, factor=0.4) # defined in sklearn.datasets
+	X1, Y1 = make_blobs(n_samples=800, centers=2, random_state=0)
+	scaling = np.max(X1)
+
+	X1 = X1/scaling
 	# gererates a data set X1 and labels Y1 with data from two circles, an inner circle 
 	# and an outer circle. The labels in Y1 are 0 or 1, indiciating the inner or outer circle.
 	# n_samples is the number of data points, noise is the noise on the data, factor is the 
 	# ratio between the radius of the inner circle to the radius of the outer circle
 
 	X2, Y2 = make_circles(n_samples=2000, noise=0.3, factor=0.4)
+	X2, Y2 = make_blobs(n_samples=8000, centers=2, random_state=0)
+	X2 = X2/scaling
 
 	frac0 = len(np.where(Y1 == 0)[0]) / float(len(Y1)) # the number of points in the inner circle
 	frac1 = len(np.where(Y1 == 1)[0]) / float(len(Y1)) # the number of points in the outer circle
@@ -35,7 +42,7 @@ def main(budget, rounds):
 	X_unif, Y_unif = make_circles(n_samples=10000, noise=0.5, factor=0.4) # Uniform test set that is used to check the stop criteria
 
 	adv = Adversary(200,'adaptive',clf)
-	adv.SetRounds(12)
+	adv.SetRounds(15)
 	adv.SetValidationSet(X2,clf.predict(X2))
 	adv.StealAPIModel(.1)
 
